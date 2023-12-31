8f3d31e7ef3fbc2c33db6c2548d3647b9ba1ece3
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-01 02:12:15 +0530

    Add files via upload

---
+++ Prac26.java
@@ -0,0 +1,20 @@
+
+public class Prac26 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int Number = 1;
+		
+		for(int a = 1; a<=x; a++) {
+			for (int b = 1; b<=a; b++) {
+				System.out.print(Number+" ");
+				Number++;
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+ 
